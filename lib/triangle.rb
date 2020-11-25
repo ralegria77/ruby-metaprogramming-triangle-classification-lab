b5ff987307d42b7265d4fdb6069bd538b64dabf1
rebec <ralegria77@gmail.com>
2020-11-25 09:31:26 -0800

    commit tree 6c5d5f40cc6acf3227caa596692c211cdd28a0cd

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,51 @@
 class Triangle
-  # write code here
+  attr_accessor :s1, :s2, :s3, :kind
+
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind
+    real_triangle
+    if s1 == s2 && s1 == s3
+      self.kind = :equilateral 
+    elsif s1 == s2 || s1 == s3 || s2 == s3
+      self.kind = :isosceles
+    else
+      self.kind = :scalene
+    end
+  end
+
+  def real_triangle
+    triangle = [(s1 + s2 > s3), (s1 + s3 > s2), (s2 + s3 > s1)]
+    [s1, s2, s3].each {|s| triangle << false if s <= 0}
+    raise TriangleError if triangle.include?(false)
+  end
+
+
+
+
+
+    # if (s1 * s2 * s3) == 0 || (s1 + s2) <= s3 || (s2 + s3) <= s1 || (s3 +s1) <= s2
+
+    # elsif s1 == s2 && s1 == s3
+    #   self.kind = :equilateral 
+
+    # elsif s1 == s2 || s1 == s3 || s2 == s3
+    #   self.kind = :isosceles
+
+    # else
+    #   self.kind = :scalene
+    # end
+    
+
+  class TriangleError < StandardError
+    def message
+      puts "Not a triangle!"
+    end
+  end
+
 end
+
